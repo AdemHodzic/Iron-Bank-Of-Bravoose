ae934012edf7be2e3e6b9761cbc77ff3e85d820d
AdemHodzic <athos.blade@gmail.com>
2018-03-05 19:57:27 +0100

    Added error handling

--- src/application/LoginView.java
+++ src/application/LoginView.java
@@ -1,20 +1,25 @@
 package application;
 
+import java.awt.RenderingHints.Key;
+
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.*;
-
 public class LoginView implements View{
 	private Scene scene;
 	private Controller controller = new Controller();
 	private UserView userView;
+	
 	private final int  WIDTH = 640;
 	private final int HEIGHT = WIDTH/12 * 9;
 	private VBox layout;
+	private Button loginButton;
 	
 	LoginView(){
 		
@@ -34,20 +39,30 @@ public class LoginView implements View{
 		userPINHBox.getChildren().addAll(userPINLabel, userPINInput);
 		
 		//Login button and register button
-		Button loginButton = new Button("Login");
+		loginButton = new Button("Login");
 		Button registerButton = new Button("Register");
 		HBox btnHBox = new HBox();
 
 		registerButton.setOnAction(e->{
-			if(controller.isNumber(userPINInput.getText())) {
 				try {
-					controller.addUser(userIDInput.getText(), Integer.parseInt(userPINInput.getText()));
-				} catch (NumberFormatException e1) {
-					e1.printStackTrace();
-				} catch (Exception e1) {
+					if(controller.isNumber(userPINInput.getText()) && !userIDInput.getText().equals("")) {
+						controller.addUser(userIDInput.getText(), Integer.parseInt(userPINInput.getText()));
+					}else if(!controller.isNumber(userPINInput.getText())){
+						ErrorView err = new ErrorView();
+						err.display("PIN NUMBER CAN ONLY CONTAIN NUMERIC CHARACTERS!"
+								+ "\nPLEASE TRY AGAIN", "INVALID PIN FORMAT");
+					}else if(userIDInput.getText().equals("")) {
+						ErrorView err = new ErrorView();
+						err.display("ID CANNOT BE BLANK!"
+								+ "\nPLEASE TRY AGAIN","INVALID PIN FORMAT");
+					}
+				} catch(NumberFormatException en) {
+					ErrorView err = new ErrorView();
+					err.display("PIN NUMBER CANNOT BE BLANK!"
+							+ "\nPLEASE TRY AGAIN","INVALID PIN FORMAT");
+				}catch (Exception e1) {
 					e1.printStackTrace();
 				}
-			}
 		});
 		
 		loginButton.setOnAction(e->{
@@ -58,8 +73,12 @@ public class LoginView implements View{
 					userView = new UserView(user);
 					scene = userView.getScene();
 				}else {
-					System.out.println("Fuck you too");
+					ErrorView err = new ErrorView();
+					err.display(("YOU HAVE ENTERED WRONG INFORMATION\nPLEASE TRY AGAIN\n" + (userPINInput.getText().length())),"WRONG INFORMATION");
 				}
+			}catch(NumberFormatException en) {
+				ErrorView err = new ErrorView();
+				err.display("YOU HAVE ENTERED WRONG PIN FORMAT\nPLEASE TRY AGAIN!","WRONG PIN INFORMATION");
 			}catch(Exception ex) {
 				ex.printStackTrace();
 			}
@@ -79,6 +98,12 @@ public class LoginView implements View{
 		makeScene();
 		if(scene==null) {
 			scene = new Scene(layout,WIDTH,HEIGHT);
+			scene.setOnKeyPressed(e->{
+				if(e.getCode().equals(KeyCode.ENTER)) {
+					loginButton.fire();
+					e.consume();
+				}
+			});
 		}
 		return scene;
 	}
